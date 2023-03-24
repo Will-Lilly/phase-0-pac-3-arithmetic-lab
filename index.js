b759c12817062f0ac78d98718dd59765ae2f4b09
Will-Lilly <126909669+Will-Lilly@users.noreply.github.com>
2023-03-24 13:33:53 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+1+80; //=> 81
+60-40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add() {
+    return "something"
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+
+function subtract() {
+    return "something"
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply() {
+    return "something"
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide() {
+    return "something"
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+function increment(n){
+    n++;
+    return n;
+  }
+
+  function decrement(n){
+    n--;
+    return n;
+  }
+
+  function makeInt(string){
+    return parseInt(string,10)
+  }
+
+  function preserveDecimal(string){
+    return parseFloat(string)
+  }
